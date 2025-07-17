9654c1c7cd50f5359485a526e9e9bf6f8d2bdf56
jwmock88 <jwmock@proton.me>
2025-07-17 13:20:38 +0100

    Added disclaimer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,13 @@
-import "./footer.css"
+import "./footer.css";
 export default function Footer() {
   return (
     <footer className="footer">
       Made by <a href="https://josh-mock.com">Josh Mock</a>. English, French,
       Spanish, and German word validation using{" "}
-      <a href="https://freedictionaryapi.com/">Free Dictionary Api</a>.
+      <a href="https://freedictionaryapi.com/">Free Dictionary Api</a>. This
+      project is a non-commercial clone inspired by
+      <a href="https://www.nytimes.com/games/wordle/index.html">Wordle</a>. It
+      is not affiliated with or endorsed by The New York Times.
     </footer>
   );
 }
