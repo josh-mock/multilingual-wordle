605b4b5fe822285ff75ab74819f1578ef4957c64
jwmock88 <jwmock@proton.me>
2025-07-17 13:06:19 +0100

    Moved styles into own directory

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./modern-normalize.css";
-import "./generics.css";
-import "./index.css";
+import "./styles/modern-normalize.css";
+import "./styles/generics.css";
+import "./styles/index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
