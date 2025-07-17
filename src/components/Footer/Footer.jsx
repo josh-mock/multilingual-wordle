edcee52e8d8d2f6f8ff0390754bb553b2b28bc54
jwmock88 <jwmock@proton.me>
2025-07-17 17:50:45 +0100

    Added space

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
       Made by <a href="https://josh-mock.com">Josh Mock</a>. English, French,
       Spanish, and German word validation using{" "}
       <a href="https://freedictionaryapi.com/">Free Dictionary Api</a>. This
-      project is a non-commercial clone inspired by
+      project is a non-commercial clone inspired by{""}
       <a href="https://www.nytimes.com/games/wordle/index.html">Wordle</a>. It
       is not affiliated with or endorsed by The New York Times.
     </footer>
